9956a045ca29ab98ef1e5a615c3911550d7de3c6
VeronicaJimenez639 <va.jimenezm@uea.edu.ec>
2026-02-22 13:01:47 -0500

    Función para validar ingreso de números decimales

--- almacen_app/main.py
+++ almacen_app/main.py
@@ -16,3 +16,14 @@ def leer_int(mensaje: str, minimo=None) -> int:   #Lee un entero desde consola y
             return valor
         except ValueError:
             print("Ingresa un número entero válido.")
+
+def leer_float(mensaje: str, minimo=None) -> float:  #Lee un float desde consola y valida un mínimo opcional.
+    while True:
+        try:
+            valor = float(input(mensaje))
+            if minimo is not None and valor < minimo:
+                print(f"Debe ser >= {minimo}.")
+                continue
+            return valor
+        except ValueError:
+            print("Ingresa un número decimal válido.")
