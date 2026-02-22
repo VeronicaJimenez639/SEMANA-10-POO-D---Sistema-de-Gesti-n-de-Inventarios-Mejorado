266900767322940e4611fef1ac377a9e0c9cd635
VeronicaJimenez639 <va.jimenezm@uea.edu.ec>
2026-02-22 11:58:15 -0500

    Se configura ruta relativa para evitar errores al ejecutar el programa desde otra carpeta

--- almacen_app/servicios/inventario.py
+++ almacen_app/servicios/inventario.py
@@ -17,3 +17,15 @@ class Inventario:
     def __init__(self, ruta_archivo: str = None):
         # Lista principal en memoria
         self.__productos: list[Producto] = []
+
+        # Construcción de ruta relativa al archivo actual
+        # Se usa ruta relativa al archivo para evitar problemas
+        # si el programa se ejecuta desde otra carpeta o terminal diferente
+        base_dir = os.path.dirname(__file__)
+        if ruta_archivo is None:
+            ruta_archivo = os.path.join(base_dir, "registros", "inventario.txt")
+
+        self.ruta_archivo = ruta_archivo
+
+        # Al iniciar el sistema, se carga el archivo automáticamente
+        self.cargar_desde_archivo()
